aff76a0efc3664ac80be0e7f75c9e789d6366a2e
Mizuho Honda <mizuho.honda@rakus-partners.co.jp>
2025-01-20 11:17:55 +0900

    3rd commit

--- src/Ex02.java
+++ src/Ex02.java
@@ -1,5 +1,9 @@
 public class Ex02 {
     public static void main(String[] args) {
+        String masage = "I have a dream";
+
+        System.out.println("最初のaは" + masage.indexOf("a") + "番目です");
+        
         
     }
 
