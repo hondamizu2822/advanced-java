af5c3e4fc08fd751667acbafc85bba0f77326046
Mizuho Honda <mizuho.honda@rakus-partners.co.jp>
2025-01-20 10:20:58 +0900

    first commit

---
+++ src/Ex01.java
@@ -0,0 +1,9 @@
+public class Ex01 {
+    public static void main(String[] args) {
+        String masage = "I have a dream today";
+        int stringLength = masage.length();
+
+        System.out.println(stringLength + "文字です");
+    }
+
+}
